d3c788756962db89fda4ba49400926ea9626b48d
VirginieMournetas <contact@virginie-mournetas.fr>
2020-12-05 15:03:39 +0100

    fixed typos

--- app.server.Exp_homemade.R
+++ app.server.Exp_homemade.R
@@ -166,11 +166,6 @@ output$Exp_homemade_table_uniqueProt <- DT::renderDataTable({
 #### PLOTS #### 
 
 #### PCA - ALL GENE #### 
-withProgress(message = 'Generating plots', detail = "part 0", value = 0, {
-  for (i in 1:3) {
-    # Each time through the loop, add another row of data. This a stand-in
-    # for a long-running computation.
-    
 
 output$Exp_homemade_PCA.mRNA <- renderPlotly({
   
